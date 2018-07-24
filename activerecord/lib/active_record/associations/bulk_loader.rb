e2b4bf84063d1d3cd8ebafa1e9e0d83fee7cb51a
Lachlan Sylvester <lachlan.sylvester@publicisfrontfoot.com.au>
2018-07-24 15:40:32 +1000

    bulk loading for nested associations

--- activerecord/lib/active_record/associations/bulk_loader.rb
+++ activerecord/lib/active_record/associations/bulk_loader.rb
@@ -3,18 +3,25 @@
 module ActiveRecord
   module Associations
     class BulkLoader
-      def initialize(records, associations)
+      def initialize(records, associations, path=[])
         @records = records
         @associations = associations
+        @path = path
         @preloader = Preloader.new
       end
 
       def loads?(association)
-        @associations.include?(association)
+        associations_for_path = @path.inject(@associations){ |result, a| result.grep(Hash).map{ |h| h[a]}.compact }
+        associations_for_path.include?(association) || associations_for_path.grep(Hash).any?{ |h| h.key?(association)}
       end
 
       def load(association)
-        @preloader.preload(@records, association)
+        @preloader.preload(@records, @path.reverse.inject(association){ |result, a| {a => result} }).tap do |preloaders|
+          bulk_loader = BulkLoader.new(@records, @associations, @path + [association])
+          preloaders.first.preloaded_records.each do |record|
+            record.bulk_loader = bulk_loader
+          end
+        end
       end
     end
   end
--- activerecord/test/cases/associations/bulk_test.rb
+++ activerecord/test/cases/associations/bulk_test.rb
@@ -1,13 +1,14 @@
 # frozen_string_literal: true
 
 require "cases/helper"
+require "models/author"
 require "models/post"
 require "models/comment"
 
 class BulkAssociationsTest < ActiveRecord::TestCase
-  fixtures :posts, :comments
+  fixtures :posts, :comments, :authors
 
-  test "bulk loading" do
+  test "bulk loading single has_many association" do
     posts = Post.bulk_load(:comments)
 
     assert_queries(1) do
@@ -17,6 +18,24 @@ class BulkAssociationsTest < ActiveRecord::TestCase
     assert_queries(1) do
       posts.first.comments.to_a
       posts.second.comments.to_a
+    end
+  end
+
+  test "bulk loading nested association" do
+    authors = Author.bulk_load(posts: :comments)
+
+    assert_queries(1) do
+      authors.load
+    end
+
+    assert_queries(1) do
+      authors.first.posts.to_a
+      authors.second.posts.to_a
+    end
+
+    assert_queries(1) do
+      authors.first.posts.first.comments.to_a
+      authors.second.posts.first.comments.to_a
     end
 
   end
