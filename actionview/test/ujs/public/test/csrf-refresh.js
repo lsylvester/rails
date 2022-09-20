8e3d8c09ce4a8bdc63f55b90b67612f3bd3ba5d0
Lachlan Sylvester <lachlan.sylvester@3plearning.com>
2022-09-20 12:27:28 +1000

    upgrade to QUnit 2 for ujs tests

--- actionview/test/ujs/public/test/csrf-refresh.js
+++ actionview/test/ujs/public/test/csrf-refresh.js
@@ -1,8 +1,9 @@
 (function() {
 
-module('csrf-refresh', {})
+QUnit.module('csrf-refresh', {})
 
-asyncTest('refresh all csrf tokens', 1, function() {
+QUnit.test('refresh all csrf tokens', function(assert) {
+  const done = assert.async()
   var correctToken = 'cf50faa3fe97702ca1ae'
 
   var form = $('<form />')
@@ -17,8 +18,8 @@ asyncTest('refresh all csrf tokens', 1, function() {
   $.rails.refreshCSRFTokens()
   currentToken = $('#qunit-fixture #authenticity_token').val()
 
-  start()
-  equal(currentToken, correctToken)
+  done()
+  assert.equal(currentToken, correctToken)
 })
 
 })()
--- actionview/test/ujs/public/test/csrf-token.js
+++ actionview/test/ujs/public/test/csrf-token.js
@@ -1,27 +1,30 @@
 (function() {
 
-module('csrf-token', {})
+QUnit.module('csrf-token', {})
 
-asyncTest('find csrf token', 1, function() {
+QUnit.test('find csrf token', function(assert) {
+  const done = assert.async()
   var correctToken = 'cf50faa3fe97702ca1ae'
 
   $('#qunit-fixture').append('<meta name="csrf-token" content="' + correctToken + '"/>')
 
   currentToken = $.rails.csrfToken()
 
-  start()
-  equal(currentToken, correctToken)
+  done()
+  assert.equal(currentToken, correctToken)
 })
 
-asyncTest('find csrf param', 1, function() {
+QUnit.test('find csrf param', function(assert) {
+  const done = assert.async()
+
   var correctParam = 'authenticity_token'
 
   $('#qunit-fixture').append('<meta name="csrf-param" content="' + correctParam + '"/>')
 
   currentParam = $.rails.csrfParam()
 
-  start()
-  equal(currentParam, correctParam)
+  done()
+  assert.equal(currentParam, correctParam)
 })
 
 })()
